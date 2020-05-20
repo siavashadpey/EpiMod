32e704aa47868dd47aed3c011d1ead870f96f1b5
sia <sia.shadpey@mail.utoronto.ca>
2020-05-20 09:55:28 -0400

    added more seir eqn unit tests

--- epimod/tests/seir_test.py
+++ epimod/tests/seir_test.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 from epimod.eqn.seir import Seir
 
+delta = 1E-7
 class TestSeir(unittest.TestCase):
 
 	def test_constructor(self):
@@ -25,8 +26,8 @@ class TestSeir(unittest.TestCase):
 					 	   3.3*u[1] - 1*u[2],
 					 	   1*u[2]])
 		
-		self.assertAlmostEqual(np.sum(s), 0.0, 1E-7)
-		np.testing.assert_almost_equal(s, s_act, 1E-7)
+		self.assertAlmostEqual(np.sum(s), 0.0, delta)
+		np.testing.assert_almost_equal(s, s_act, delta)
 
 	def test_source_state_grad(self):
 		u = np.array([100., 90., 110., 220.])
@@ -38,7 +39,7 @@ class TestSeir(unittest.TestCase):
 		(s, ds_du, ds_dp) = eqn.source(u=u, is_grad_needed = True)
 
 		epsi = 1E-1
-		
+
 		for i in range(eqn.n_components()):
 			u_p1 = np.copy(u)
 			u_p1[i] += epsi
@@ -47,7 +48,7 @@ class TestSeir(unittest.TestCase):
 			u_m1 = np.copy(u)
 			u_m1[i] -= epsi
 			s_m1 = eqn.source(u = u_m1)
-			np.testing.assert_almost_equal(ds_du[:,i], (s_p1 - s_m1)/(2*epsi), 1E-7)
+			np.testing.assert_almost_equal(ds_du[:,i], (s_p1 - s_m1)/(2*epsi), delta)
 
 	def test_source_param_grad(self):
 		u = np.array([100., 90., 110., 220.])
@@ -66,21 +67,41 @@ class TestSeir(unittest.TestCase):
 		s_p1 = eqn.source(u=u)
 		eqn = Seir(beta - epsi, sigma, gamma)
 		s_m1 = eqn.source(u=u)
-		np.testing.assert_almost_equal(ds_dp[:,0], (s_p1 - s_m1)/(2*epsi), 1E-7)
+		np.testing.assert_almost_equal(ds_dp[:,0], (s_p1 - s_m1)/(2*epsi), delta)
 
 		# sigma gradient
 		eqn = Seir(beta, sigma + epsi, gamma)
 		s_p1 = eqn.source(u=u)
 		eqn = Seir(beta, sigma - epsi, gamma)
 		s_m1 = eqn.source(u=u)
-		np.testing.assert_almost_equal(ds_dp[:,1], (s_p1 - s_m1)/(2*epsi), 1E-7)
+		np.testing.assert_almost_equal(ds_dp[:,1], (s_p1 - s_m1)/(2*epsi), delta)
 
 		# gamma gradient
 		eqn = Seir(beta, sigma, gamma + epsi)
 		s_p1 = eqn.source(u=u)
 		eqn = Seir(beta, sigma, gamma - epsi)
 		s_m1 = eqn.source(u=u)
-		np.testing.assert_almost_equal(ds_dp[:,2], (s_p1 - s_m1)/(2*epsi), 1E-7)
+		np.testing.assert_almost_equal(ds_dp[:,2], (s_p1 - s_m1)/(2*epsi), delta)
+
+	def test_output(self):
+		u = np.array([100., 90., 110., 220.])
+
+		eqn = Seir(beta=2.2,gamma=1,sigma=3.3)
+		s = eqn.source(u=u)
+		f = eqn.output(u=u)
+
+		self.assertAlmostEqual(f, u[2], delta)
+
+	def test_output_param_grad(self):
+		u = np.array([100., 90., 110., 220.])
+
+		eqn = Seir(beta=2.2,gamma=1,sigma=3.3)
+		s = eqn.source(u=u)
+		du_dp = np.random.random((eqn.n_components(), eqn.n_parameters()))
+		(f, df_dp) = eqn.output(u=u, du_dp = du_dp)
+
+		np.testing.assert_almost_equal(df_dp, du_dp[2,:], delta)
+
 
 if __name__ == '__main__':
 	unittest.main()
