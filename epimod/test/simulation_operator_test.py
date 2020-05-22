6a35b1223e4f2f960ebe613a96c0eb52ee935dd3
sia <sia.shadpey@mail.utoronto.ca>
2020-05-22 16:11:39 -0400

    renamed dir tests -> test

---
+++ epimod/test/simulation_operator_test.py
@@ -0,0 +1,75 @@
+import unittest
+from theano.tests import unittest_tools as utt
+
+import numpy as np
+import theano as theano
+
+from epimod.model.simulation_operator import CachedSimulation
+from epimod.model.simulation_operator import ModelOp
+from epimod.model.simulation_operator import ModelGradOp
+from epimod.eqn.seir import Seir
+from epimod.solver.ode_solver.rk_solver import RKSolver
+
+theano.config.exception_verbosity= 'high'
+
+decimal = 7
+class CachedSEIRSimulation(CachedSimulation):
+
+	def _update_parameters():
+		x = np.array(self._eqn_parameters,dtype=np.float64)
+		(beta, sigma, gamma) = x
+		eqn = self._ode_solver.eqn
+		eqn.beta = beta
+		eqn.sigma = sigma
+		eqn.gamma = gamma
+
+class TestModelOp(utt.InferShapeTester):
+	rng = np.random.RandomState(43)
+
+	def setUp(self):
+		super(TestModelOp, self).setUp()
+		self.setUpModel()
+
+	def setUpModel(self):
+
+		# set ode solver
+		ti = 0
+		tf = 20
+		n_steps = tf
+		rk = RKSolver(ti, tf, n_steps)
+
+		rk.output_frequency = 1
+		rk.set_output_storing_flag(True)
+		rk.equation = Seir()
+
+		n_pop = 7E6
+		u0 = np.array([n_pop - 1, 0, 1, 0])
+		u0 /= n_pop
+		rk.set_initial_condition(u0)
+
+		# set cached_sim object
+		cached_sim = CachedSimulation(rk)
+		cached_sim.set_gradient_flag(False)
+
+		# set theano model op object
+		self.op_class = ModelOp(cached_sim)
+
+	def test_perform(self):
+		b = theano.tensor.dscalar('myvar0')
+		s = theano.tensor.dscalar('myvar1')
+		g = theano.tensor.dscalar('myvar2')
+		f = theano.function([b, s, g], self.op_class((b, s, g)))
+		s_val = 1./5.2 
+		g_val = 1./2.28
+		b_val = 2.13*g_val
+		out = f(b_val, s_val, g_val)
+		out_act = np.array([1.42857143e-07, 1.01439369e-07, 8.65162795e-08, 8.46869028e-08,
+  							 8.97221828e-08, 9.87819861e-08, 1.10634869e-07, 1.24821306e-07,
+  							 1.41261498e-07, 1.60072825e-07, 1.81486334e-07, 2.05810192e-07,
+  							 2.33415645e-07, 2.64733988e-07, 3.00259217e-07, 3.40553892e-07,
+  							 3.86257136e-07, 4.38094351e-07, 4.96888522e-07, 5.63573190e-07,
+  							 6.39207233e-07])
+		assert np.allclose(out_act, out)
+
+if __name__ == '__main__':
+	unittest.main()
