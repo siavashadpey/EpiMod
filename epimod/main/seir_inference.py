31dba4385cb804b34354134973f097200060bcd7
Siavosh Shadpey <sia.shadpey@mail.utoronto.ca>
2020-06-09 10:17:42 -0400

    changed driver file

--- epimod/main/seir_inference.py
+++ epimod/main/seir_inference.py
@@ -3,15 +3,16 @@ import math
 import os
 import datetime as dt
 
+import time
+
 import numpy as np
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
-import theano as theano
-
 import pymc3 as pm
 import arviz as az
+import pandas as pd
 
 from epimod.model.simulation_operator import CachedSimulation
 from epimod.model.simulation_operator import ModelOp
@@ -22,7 +23,10 @@ import epimod.data.read_region_data as data_fetcher
 
 class CachedSEIRSimulation(CachedSimulation):
 	def _update_parameters(self):
-		(beta, sigma, gamma, kappa, tint) = np.array(self._eqn_parameters, dtype=np.float64)
+		(beta, sigma, gamma, kappa, tint) = self._eqn_parameters
+		#print(beta, sigma, gamma, kappa, tint)
+		import sys
+		sys.stdout.flush()
 		eqn = self._ode_solver.equation
 		eqn.beta = beta
 		eqn.sigma = sigma
@@ -38,10 +42,8 @@ class RKSolverSeir(RKSolver):
 		outs = super().get_outputs()
 		if self._is_output_stored:
 			if self._is_output_grad_needed:
-				#print(outs[1])
 				y = np.diff(outs[1], prepend=0)
 				y[y<1.E-14] = 1.E-14
-				#print(y)
 				return (self._time_array, y, np.diff(outs[2], prepend=0))
 			else:
 				y = np.diff(outs[1], prepend=0)
@@ -50,7 +52,18 @@ class RKSolverSeir(RKSolver):
 		else:
 			error("output is not stored")
 
-def run(region, folder, load_trace=False, compute_sim=True):
+def single_simulation(tup, rk):
+	(beta, sigma, gamma, kappa, tint) = tup
+	rk.equation.beta = beta
+	rk.equation.sigma = sigma
+	rk.equation.gamma = gamma
+	rk.equation.kappa = kappa
+	rk.equation.tint = tint
+	rk.solve()
+	(_, y_sim) = rk.get_outputs()	
+	return y_sim
+
+def run(region, folder, load_trace=False, compute_sim=True, plot_posterior_dist = True):
 
 	print("started ... " + region)
 
@@ -61,7 +74,7 @@ def run(region, folder, load_trace=False, compute_sim=True):
 	(t_obs, datetimes, y_obs, n_pop, shutdown_day, u0, _) = data_fetcher.read_region_data(folder, region)
 	y_obs = y_obs.astype(np.float64)
 	u0 = u0.astype(np.float64)
-	
+
 	# set eqn
 	eqn = Seir()
 	eqn.tau = shutdown_day
@@ -69,10 +82,11 @@ def run(region, folder, load_trace=False, compute_sim=True):
 	# set ode solver
 	ti = t_obs[0]
 	tf = t_obs[-1]
-	n_steps = tf - ti
+	m = 2
+	n_steps = m*(tf - ti)
 	rk = RKSolverSeir(ti, tf, n_steps)
-	
-	rk.output_frequency = 1
+	rk.rk_type = "explicit_euler"
+	rk.output_frequency = m
 	rk.set_output_storing_flag(True)
 	rk.equation = eqn
 	
@@ -83,6 +97,17 @@ def run(region, folder, load_trace=False, compute_sim=True):
 	# sample posterior
 	with pm.Model() as model:
 		# set prior distributions
+		#beta  = pm.Lognormal('beta',  mu = math.log(0.4/n_pop), sigma = 0.4)
+		#sigma = pm.Lognormal('sigma', mu = math.log(0.3), sigma = 0.5)
+		#gamma = pm.Lognormal('gamma', mu = math.log(0.25), sigma = 0.5)
+		#kappa = pm.Lognormal('kappa', mu = math.log(0.1), sigma = 0.5)
+
+		#beta  = pm.Normal('beta',  mu = 0.4/n_pop, sigma = 0.06/n_pop)
+		#sigma = pm.Normal('sigma', mu = 0.6, sigma = 0.1)
+		#gamma = pm.Normal('gamma', mu = 0.3, sigma = 0.07)
+		#kappa = pm.Normal('kappa', mu = 0.5, sigma = 0.1)
+		#tint = pm.Lognormal('tint', mu = math.log(30), sigma = 1)
+
 		beta  = pm.Lognormal('beta',  mu = math.log(0.3/n_pop), sigma = 0.5)
 		sigma = pm.Lognormal('sigma', mu = math.log(0.05), sigma = 0.6)
 		gamma = pm.Lognormal('gamma', mu = math.log(0.05), sigma = 0.6)
@@ -101,70 +126,57 @@ def run(region, folder, load_trace=False, compute_sim=True):
 		
 		if not load_trace:
 			# sample posterior distribution and save trace
-			trace = pm.sample(draws=20, tune=10, cores=4, chains=4, nuts_kwargs=dict(target_accept=0.9), init='advi+adapt_diag') # using NUTS sampling
+			draws = 20 #1000
+			tune = 10 #500
+			trace = pm.sample(draws=draws, tune=tune, cores=4, chains=4, nuts_kwargs=dict(target_accept=0.9), init='advi+adapt_diag') # using NUTS sampling
 			# save trace
 			pm.backends.text.dump(region + os.path.sep, trace)
 		else:
 			# load trace
 			trace = pm.backends.text.load(region + os.path.sep)		
 		
+		if plot_posterior_dist:
 		# plot posterior distributions of all parameters
-		data = az.from_pymc3(trace=trace)
-		pm.plots.traceplot(data, legend=True)
-		plt.savefig(region + os.path.sep + "trace_plot.pdf")
-		az.plot_posterior(data,  hdi_prob = 0.95)
-		plt.savefig(region + os.path.sep + "post_dist.pdf")
+			data = az.from_pymc3(trace=trace)
+			pm.plots.traceplot(data, legend=True)
+			plt.savefig(region + os.path.sep + "trace_plot.pdf")
+			az.plot_posterior(data,  hdi_prob = 0.95)
+			plt.savefig(region + os.path.sep + "post_dist.pdf")
 
 		if compute_sim:
-			rk.set_output_gradient_flag(False)
+			#rk.set_output_gradient_flag(False)
 			n_predictions = 7
 			rk.final_time = rk.final_time + n_predictions
-			rk.n_steps = rk.n_steps + n_predictions
+			rk.n_steps = rk.n_steps + m*n_predictions
 
-			betas = trace.get_values('beta')
-			sigmas = trace.get_values('sigma')
-			gammas = trace.get_values('gamma')
-			kappas = trace.get_values('kappa')
-			tints = trace.get_values('tint')
-
-			n_samples = betas.shape[0]
-			y_sims = np.zeros((n_samples, eqn.n_outputs(), rk.n_steps + 1))
-			# TODO: parallelize
-			for i in range(n_samples):
-				eqn.beta = betas[i]
-				eqn.sigma = sigmas[i]
-				eqn.gamma = gammas[i]
-				eqn.kappa = kappas[i]
-				eqn.tint = tints[i]
-
-				rk.solve()
-				(_, y_sim_i) = rk.get_outputs()
-				y_sims[i,:,:] = y_sim_i
+			y_sims = pm.sample_posterior_predictive(trace)['y_sim'][:,0,:]
+			np.savetxt(region + os.path.sep + "y_sims.csv", y_sims, delimiter = ',')
+			mean_y = np.mean(y_sims,axis=0)
+			upper_y = np.percentile(y_sims,q=97.5,axis=0)
+			lower_y = np.percentile(y_sims,q=2.5,axis=0)
 	
-			# compute mean and 95% credible interval
-			mean_y = np.mean(y_sims[:,0,:], axis=0)
-			lower_y = np.percentile(y_sims[:,0,:],q=2.5,axis=0)
-			upper_y = np.percentile(y_sims[:,0,:],q=97.5,axis=0)
-
 			# plots
 			dates = [dt.datetime.strptime(date, "%Y-%m-%d").date() for date in datetimes]
 			pred_dates = dates + [dates[-1] + dt.timedelta(days=i) for i in range(1,1 + n_predictions)]
 
+			np.savetxt(region + os.path.sep + "y_obs.csv", y_obs, delimiter = ',')
+
+			dates_csv = pd.DataFrame(pred_dates).to_csv(region + os.path.sep + 'dates.csv', header=False, index=False)
 			# linear plot
-			fig = plt.figure(figsize=(7, 7))
-			ax = fig.add_subplot(111, xlabel='x', ylabel='y')
-			ax.plot(dates, y_obs, 'x', color='k')
+			font_size = 12
+			fig, ax = plt.subplots(figsize=(10, 10))
+			ax.plot(dates, y_obs, 'x', color='k', label='reported data')
 			import matplotlib.dates as mdates
 			ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %b'))
-			plt.title(region[0].upper() + region[1:].lower() + "'s daily infections")
-			plt.xlabel('Date')
-			plt.ylabel('New daily infections')
+			ax.xaxis.set_major_locator(mdates.DayLocator(bymonthday=(1,15)))
+			plt.title(region[0].upper() + region[1:].lower() + "'s daily infections", fontsize = font_size)
+			plt.xlabel('Date', fontsize = font_size)
+			plt.ylabel('New daily infections', fontsize = font_size)
+			ax.tick_params(axis='both', which='major', labelsize=10)
 
 			# plot propagated uncertainty
 			plt.plot(pred_dates, mean_y, color='g', lw=2, label='mean')
 			plt.fill_between(pred_dates, lower_y, upper_y, color='darkseagreen', label='95% credible interval')
-			#plt.plot(
-			#plt.plot(pred_dates, upper_y, '--', lw=2, color='g')
 			plt.legend(loc='upper left')
 			fig.autofmt_xdate()
 			plt.savefig(region + os.path.sep + "linear.pdf")
@@ -181,14 +193,16 @@ def main():
 	parser.add_argument('--region', '-r', default='canada', help='region of interest')
 	parser.add_argument('--load_trace', '-l', action='store_true', default=False, help='flag indicating to load trace')
 	parser.add_argument('--no_propagate', '-np', action='store_true', default=False, help='flag indicating not to perform UQ or not')
+	parser.add_argument('--no_post_plot', '-npp', action='store_true', default=False, help='flag indicating not to plot posterior distribution')
 
 	args = parser.parse_args()
 
 	folder = args.folder
 	region = args.region.lower()
 	load_trace = args.load_trace
+	plot_post = not args.no_post_plot
 	propagate = not args.no_propagate
-	run(region, folder, load_trace, propagate)
+	run(region, folder, load_trace, propagate, plot_post)
 
 if __name__ == '__main__':
 	main()
